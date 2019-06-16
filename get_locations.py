5cd6970f339b8e12c1462fd9bcbeee1150debb06
jashrathod0 <jashrathod0@gmail.com>
2019-06-16 16:49:07 +0530

    heatmap 1.0

---
+++ get_locations.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import googlemaps
+import gmaps as gm
+
+API_KEY = 'AIzaSyBfiH9Gy-YDMv0lYDDwhAOioVasVl0NJPg'
+gm = googlemaps.Client(key=API_KEY)
+
+data = pd.read_csv('MetroManilaSpatialAccidentData.csv', encoding = "ISO-8859-1", low_memory=False, index_col = 'Key')
+data = data.fillna('')                   # fill empty entries with ''
+print(list(data))                        # print Variable Name
+# data.head()                              # show some data
+
+[maxRow,maxCol]=data.shape
+# maxRow,maxCol
+
+
+def Geocode(query):
+    # do geocoding
+    try:
+        geocode_result = gm.geocode(query)[0]
+        latitude = geocode_result['geometry']['location']['lat']
+        longitude = geocode_result['geometry']['location']['lng']
+        return latitude, longitude
+    except IndexError:
+        return 0
+
+
+def GeocodeStreetLocationCity(data):
+    lat = []  # initialize latitude list
+    lng = []  # initialize longitude list
+    start = data.index[0]  # start from the first data
+    end = data.index[maxRow - 1]  # end at maximum number of row
+    for i in range(start, end + 1, 1):  # iterate all rows in the data
+        isSuccess = True  # initial Boolean flag
+        query = data.Street[i] + ' ' + data.Location[i] + ' ' + data.City[
+            i]  # try set up our query street-location-city
+        result = Geocode(query)
+        if result == 0:  # if not successful,
+            query = data.Location[i] + ' ' + data.City[i]  # try set up another query location-city
+            result = Geocode(query)
+            if result == 0:  # if still not successful,
+                query = data.Street[i] + ' ' + data.City[i]  # try set up another query street-city
+                result = Geocode(query)
+                if Geocode(query) == 0:  # if still not successful,
+                    isSuccess = False  # mark as unsuccessful
+                    print(i, 'is failed')
+                else:
+                    print(i, result)
+            else:
+                print(i, result)
+        else:
+            print(i, result)
+        if isSuccess == True:  # if geocoding is successful,
+            # store the results
+            lat.append(result[0])  # latitude
+            lng.append(result[1])  # longitude
+    return lat, lng
+
+
+# call the geocoding function
+[lat,lng]=GeocodeStreetLocationCity(data)
+
+# we put the list of latitude,longitude into pandas data frame
+df = pd.DataFrame(
+    {'latitude': lat,
+     'longitude': lng
+    })
+
+# do geocode for the whole mega city
+geocode_result = gm.geocode('Metro Manila')[0]  # change the name into your city of interest
+
+# get the center of the city
+center_lat=geocode_result['geometry']['location']['lat']
+center_lng=geocode_result['geometry']['location']['lng']
+print('center=',center_lat,center_lng)
+
+# get the bounding box of the city i.e. Metro Manila
+bounding_box = geocode_result['geometry']['bounds']
+lat_boundary = [bounding_box['southwest']['lat'], bounding_box['northeast']['lat']]
+lng_boundary = [bounding_box['southwest']['lng'], bounding_box['northeast']['lng']]
+print('boundary:',lat_boundary,lng_boundary)
+
+# remove the list that is outside of the city bounding box
+mask=(df.latitude>lat_boundary[0])  & \
+     (df.latitude<lat_boundary[1])  & \
+     (df.longitude>lng_boundary[0]) & \
+     (df.longitude<lng_boundary[1])
+df=df[mask]
+
+# add frequency of accidents in the location
+df['weight']=1
+
+# save into csv file
+df.to_csv('locations.csv')
+print('saved gocoded locations to "locations.csv"')
+
+
+
+
---
+++ heatmap.py
@@ -0,0 +1,76 @@
+
+import matplotlib.pyplot as plt
+
+
+# new_york_coordinates = (40.75, -74.00)
+# m = gmaps.figure(center=new_york_coordinates, zoom_level=12)
+# gmaps.display(m)
+
+
+
+# import gmplot
+#
+# # GoogleMapPlotter return Map object
+# # Pass the center latitude and
+# # center longitude
+# # gmap1 = gmplot.GoogleMapPlotter(30.3164945, 78.03219179999999, 13)
+#
+# # Pass the absolute path
+# # gmap1.draw("map_coordinates.html")
+#
+# # from_geocode method return the
+# # latitude and longitude of given location .
+#
+# gmaps = googlemaps.Client(key='AIzaSyAi_FN_73TGJA44u5ZCK4DFczyFMI8IA5Y')
+# geocode_result = gmaps.geocode('100 Broadway Street, Missoula, MT')
+#
+# geom = geocode_result[0]['geometry']
+# loc = geom['location']
+# lat = loc['lat']
+# lng = loc['lng']
+#
+# gmap2 = gmplot.GoogleMapPlotter.from_geocode("Missoula")
+#
+# gmap2.draw("map_location.html")
+
+
+
+import googlemaps, gmplot, webbrowser, os, json
+
+api_key = 'AIzaSyAtah9mXN0HPOfq-EaxsCO8rbcKIMIyauM'
+gmaps = googlemaps.Client(key=api_key)  # AIzaSyAtah9mXN0HPOfq-EaxsCO8rbcKIMIyauM
+geocode_result = gmaps.geocode('100 Broadway Street, Missoula, MT')
+
+# #######
+# gmap = gmplot.GoogleMapPlotter.from_geocode("Missoula")    # IF THIS LINE IS HERE, IT THROWS THE ERROR
+# #######
+
+geom = geocode_result[0]['geometry']
+loc = geom['location']
+lat = loc['lat']
+lng = loc['lng']
+
+#######
+gmap = gmplot.GoogleMapPlotter.from_geocode("Missoula")    # IF THIS LINE IS HERE, IT WORKS
+#######
+
+hidden_gem_lat, hidden_gem_lon = lat,lng
+gmap.marker(hidden_gem_lat, hidden_gem_lon, 'cornflowerblue')
+
+# Draw
+gmap.draw("map_missoula.html")
+
+filename = 'file:///'+os.getcwd()+'/' + 'map_missoula.html'
+webbrowser.open_new_tab(filename)
+
+
+def from_geocode(cls, location_string, api_key, zoom=13):
+    lat, lng = cls.geocode(location_string, api_key)
+    return cls(lat, lng, zoom, api_key)
+
+
+# def geocode(self, location_string, api_key):
+#     geo_code = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address="%s"&key=%s' % (location_string, api_key))
+#     geo_code = json.loads(geo_code.text)
+#     latlng_dict = geo_code['results'][0]['geometry']['location']
+#     return latlng_dict['lat'], latlng_dict['lng']
